5f471b5a9d964512ec5e1c897e62e49492dae6d2
kevinm9 <kevin.mosquera.coronel@gmail.com>
2021-09-26 15:03:40 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-fbri1n
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-fbri1n)
---
+++ index.js
@@ -0,0 +1,7 @@
+// Import stylesheets
+import './style.css';
+import 'http://cdn.sencha.io/ext-4.2.0-gpl/ext-all.js';
+import '//extjs.cachefly.net/ext-4.2.0-gpl/ext-all.js';
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
